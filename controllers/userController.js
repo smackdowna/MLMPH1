2e24ca7cf91e0fa58081f323eee497994bf012b7
Rishi <rishiraj1096@gmail.com>
2023-12-13 21:35:37 +0530

    frogot password

--- controllers/userController.js
+++ controllers/userController.js
@@ -222,50 +222,31 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("User not found", 404));
   }
 
-  // Get ResetPassword Token
-  const resetToken = user.getResetPasswordToken();
-
-  await user.save({ validateBeforeSave: false });
-
-  const resetPasswordUrl = `${req.protocol}://${req.get(
-    "host"
-  )}/api/v1/password/reset/${resetToken}`;
-
-  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
-
-  try {
-    await sendEmail({
-      email: user.email,
-      subject: `ShridhamGhosala Password Recovery`,
-      message,
-    });
-
-    res.status(200).json({
-      success: true,
-      message: `Email sent to ${user.email} successfully`,
-    });
-  } catch (error) {
-    user.resetPasswordToken = undefined;
-    user.resetPasswordExpire = undefined;
-
-    await user.save({ validateBeforeSave: false });
-
-    return next(new ErrorHandler(error.message, 500));
+  if (!req.body.password) {
+    return next(new ErrorHandler("Please enter password", 400));
   }
+
+  if (!req.body.confirmPassword) {
+    return next(new ErrorHandler("Please enter confirm password", 400));
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password does not password", 400));
+  }
+
+  user.password = req.body.password;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: `Password changed successfully`,
+  });
 });
 
 // Reset Password
 exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
-  // creating token hash
-  const resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(req.params.token)
-    .digest("hex");
-
-  const user = await User.findOne({
-    resetPasswordToken,
-    resetPasswordExpire: { $gt: Date.now() },
-  });
+  const user = await User.findOne({ email: req.body.email });
 
   if (!user) {
     return next(
@@ -281,8 +262,6 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
   }
 
   user.password = req.body.password;
-  user.resetPasswordToken = undefined;
-  user.resetPasswordExpire = undefined;
 
   await user.save();
 
