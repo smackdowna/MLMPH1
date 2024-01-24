5398e7ed31d8e2fb1e0ba1626d8bbbcc105a4095
Rishi <rishiraj1096@gmail.com>
2024-01-24 13:22:23 +0530

    frontend url whitlist

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const errorMiddleware = require("./middlewares/error");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 app.use(
-    cors({
-      origin: 'https://shreegoudham.com',
-      credentials: true,
-      methods: ["GET", "POST", "DELETE", "PUT"],
-    })
-  );
+  cors({
+    origin: ["https://shreegoudham.com", "https://www.shreegoudham.com"],
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+  })
+);
 
-
-app.use(express.json())
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-
-
 //Route Imports
 const user = require("./routes/userRoute");
-app.use("/api/v1",user);
+app.use("/api/v1", user);
 
 app.use(errorMiddleware);
-
-
-
 
 module.exports = app;
